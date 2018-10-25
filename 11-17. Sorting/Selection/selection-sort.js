a2f5b901f8f02a880c8d5b7a7fb85355661aac9d
Tam Le <philosocode@gmail.com>
2018-10-25 08:05:37 -0600

    Add Elementary sorts

---
+++ 11-17. Sorting/Selection/selection-sort.js
@@ -0,0 +1,39 @@
+// LEGACY VERSION (non ES2015 syntax)
+function sselectionSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    var lowest = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      //SWAP!
+      var temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+// ES2015 VERSION
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) =>
+    ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+
+  return arr;
+}
+
+selectionSort([0, 2, 34, 22, 10, 19, 17]);
