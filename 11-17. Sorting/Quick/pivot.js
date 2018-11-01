1113ef6ea66d214087168546a65f5b1809932c09
Tam Le <philosocode@gmail.com>
2018-11-01 07:58:11 -0600

    Quick & Merge

---
+++ 11-17. Sorting/Quick/pivot.js
@@ -0,0 +1,46 @@
+// First Version
+function pivot(arr, start=0, end=arr.length+1){
+  function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  var pivot = arr[start];
+  var swapIdx = start;
+
+  for(var i = start + 1; i < arr.length; i++){
+    if(pivot > arr[i]){
+      swapIdx++;
+      swap(arr,swapIdx,i);
+    }
+  }
+  swap(arr,start,swapIdx);
+  return swapIdx;
+}
+
+// Version with ES2015 Syntax
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+pivot([4,8,2,1,5,7,6,3])
+
+
